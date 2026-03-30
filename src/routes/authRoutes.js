883cbb673284ff21e9f6b97558b451f847e348e4
Dana-Hamshuo <maintanceticketsystem@gmail.com>
2026-03-30 17:40:28 +0300

    update

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const { loginValidation } = require("../validators/auth/login.validation");
 const validate = require("../middlewares/validationMiddleware");
-
+const router = require("express").Router()
 router.post(
   "/login",
   loginValidation,
