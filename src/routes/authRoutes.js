abb3f45249bc70779ca38cc1fe765302f0a3ef1f
Dana-Hamshuo <maintanceticketsystem@gmail.com>
2026-04-04 20:55:30 +0300

    update

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = require("express").Router()
 const { loginValidation } = require("../validators/auth/login.validation");
 const validate = require("../middlewares/validationMiddleware")
 const authController = require("../controllers/authController.js")
+const protect = require("../middlewares/authMiddleware.js")
 router.post(
   "/login",
   loginValidation,
